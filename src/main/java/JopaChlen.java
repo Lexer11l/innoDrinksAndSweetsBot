781eb81c64f5926245d18ce8c807585f2d57ad1d
k.meshkov <k.meshkov@innopolis/.uj>
2017-11-03 18:24:36 +0300

    added buttons

--- src/main/java/JopaChlen.java
+++ src/main/java/JopaChlen.java
@@ -1,6 +1,8 @@
 /**
  * Created by Kirill on 03.11.2017.
  */
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
@@ -8,6 +10,9 @@ import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JopaChlen extends TelegramLongPollingBot {
 
@@ -38,6 +43,8 @@ public class JopaChlen extends TelegramLongPollingBot {
         if (message != null && message.hasText()) {
             if (message.getText().equals("/help"))
                 sendMsg(message, "Привет, я робот");
+            else if (message.getText().equals("Комманда 3"))
+                sendMsg(message, "Я знаю что ответить на это");
             else
                 sendMsg(message, "Я не знаю что ответить на это");
         }
@@ -46,6 +53,37 @@ public class JopaChlen extends TelegramLongPollingBot {
     private void sendMsg(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
+
+        //
+         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+	// Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+	// Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+	// Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add("Комманда 1");
+        keyboardFirstRow.add("Комманда 2");
+
+	// Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+	// Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add("Комманда 3");
+        keyboardSecondRow.add("Комманда 4");
+
+	// Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+	// и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+//dcs`
+
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
